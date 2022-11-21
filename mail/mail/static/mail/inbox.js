13eb35d367c8eaf2336efb2a771ed5f444fb4133
Dawson Cooper <83229549+DawsonCooper@users.noreply.github.com>
2022-11-21 14:56:19 -0600

    Added emails to the inbox page, clear view when an email box it clicked, marks emails as read

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -34,7 +34,7 @@ function compose_email() {
                 body: JSON.stringify({
                     recipients: recipients,
                     subject: subject,
-                    body: body,
+                    body: body
                 })
             })
             .then(response => response.json())
@@ -63,27 +63,69 @@ function load_mailbox(mailbox) {
 
     document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
     if (mailbox == 'sent') {
-
         fetch('/emails/sent').then(response => response.json())
             .then(emails => {
                 console.log(emails);
                 emails.forEach(email => {
                     // create html to display the emails content
-                    let emailDivLeft = document.createElement('div');
-                    emailDivLeft.setAttribute('id', email.id);
-                    emailDivLeft.setAttribute('class', 'emailDiv');
+                    let emailDiv = document.createElement('div');
+                    emailDiv.setAttribute('id', email.id);
+                    emailDiv.setAttribute('class', "emailDiv")
                     let recipient = document.createElement('h4');
                     recipient.innerText = email.recipients;
                     let subject = document.createElement('h5');
                     subject.innerText = email.subject;
                     let timestamp = document.createElement('p');
                     timestamp.innerText = email.timestamp;
-                    emailDivLeft.appendChild(recipient);
-                    emailDivLeft.appendChild(subject);
-                    emailDivLeft.appendChild(timestamp);
-                    document.querySelector('#emailDiv').appendChild(emailDivLeft);
-                });
-            }).catch(error => console.log(error));
+                    emailDiv.appendChild(recipient);
+                    emailDiv.appendChild(subject);
+                    emailDiv.appendChild(timestamp);
+                    document.querySelector('#emails-view').appendChild(emailDiv);
+                    // Need to add view email function
+                    emailDiv.addEventListener('click', function() {
+
+                        //USER SHOULD BE ABLE TO SEE THE EMAIL IN IT ENTIRETY AND ARCHIVE IT 
+                        document.querySelector('#emails-view').style.display = 'none';
+
+
+                    });
+                }).catch(error => console.log(error));
+            })
+    }
+    if (mailbox === 'inbox') {
+        fetch('/emails/inbox').then(response => response.json())
+            .then(emails => {
+                console.log(emails);
+                emails.forEach(email => {
+                    // create html to display the emails content
+                    let emailDiv = document.createElement('div');
+                    emailDiv.setAttribute('id', email.id);
+                    emailDiv.setAttribute('class', "emailDiv")
+                    let sender = document.createElement('h4');
+                    sender.innerText = email.sender;
+                    let subject = document.createElement('h5');
+                    subject.innerText = email.subject;
+                    let timestamp = document.createElement('p');
+                    timestamp.innerText = email.timestamp;
+                    emailDiv.appendChild(sender);
+                    emailDiv.appendChild(subject);
+                    emailDiv.appendChild(timestamp);
+
+                    document.querySelector('#emails-view').appendChild(emailDiv);
+                    // Need to add view email function
+                    if (!email.read) {
+                        emailDiv.style["background-color"] = ("rgba(192, 192, 192");
+                    }
+                    emailDiv.addEventListener('click', function() {
+                        // Here i will want to display the email and have buttons to archive should also set read to true and maybe a delete button??
+                        fetch(`/emails/${email.id}`, {
+                            method: 'PUT',
+                            body: JSON.stringify({ read: true })
+                        }).catch(error => console.log(error));
+                        document.querySelector('#emails-view').style.display = 'none';
+                    });
+                })
+            })
     }
     return 0;
 }
